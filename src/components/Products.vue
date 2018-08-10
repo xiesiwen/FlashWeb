f51591eb12435976cac94e591d48b343cca95d92
xiejingwen <xiejingwen@changingedu.com>
2018-08-10 19:19:03 +0800

    增加商品请求

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="hello">
     商品
+    <ol>
+        <li v-for="product in products" :key="product.id">
+            <div>分类：{{product.get('classify')}}</div>
+            <div>名称：{{product.get('name')}}</div>
+            <div>价格：{{product.get('price')}}</div>
+        </li>
+        </ol>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Products',
-  data () {
+  name: "Products",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      products: []
+    };
+  },
+  created() {
+    var that = this;
+    var order = Bmob.Object.extend("Products");
+    var query = new Bmob.Query(order);
+    query.find({
+      success: function(results) {
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          that.products.push(object);
+        }
+      },
+      error: function(error) {
+        console.log("查询失败: " + error.code + " " + error.message);
+      }
+    });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -25,7 +49,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
